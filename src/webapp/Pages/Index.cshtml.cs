697c1802fdd1eb6e6201045a64382cdbdd630235
Adem Koylu <ademkyl@gmail.com>
2026-05-08 16:34:07 +0300

    Release v5 with ConfigMap support

--- src/webapp/Pages/Index.cshtml.cs
+++ src/webapp/Pages/Index.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace webapp.Pages;
-
 public class IndexModel : PageModel
 {
+    private readonly ILogger<IndexModel> _logger;
+
+    public string WelcomeMessage { get; set; } = "";
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnGet()
     {
-
+        WelcomeMessage = Environment.GetEnvironmentVariable("WELCOME_MESSAGE") ?? "Hello from default app configuration";
     }
 }
